39f162ec8d3931d5fac469dad74426e5b18cd886
Script <huutai1515225@gmail.com>
2023-06-28 16:37:31 +0700

    Sua home

--- Hotel/Home.Designer.cs
+++ Hotel/Home.Designer.cs
@@ -29,25 +29,27 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
-            this.user = new System.Windows.Forms.Label();
-            this.panel2 = new System.Windows.Forms.Panel();
+            this.panelMenu = new System.Windows.Forms.FlowLayoutPanel();
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
             this.button6 = new System.Windows.Forms.Button();
+            this.user = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panelCallForm = new System.Windows.Forms.Panel();
             this.panel1.SuspendLayout();
-            this.flowLayoutPanel1.SuspendLayout();
+            this.panelMenu.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
-            this.panel1.Controls.Add(this.flowLayoutPanel1);
+            this.panel1.Controls.Add(this.panelMenu);
             this.panel1.Controls.Add(this.user);
             this.panel1.Controls.Add(this.pictureBox1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
@@ -56,38 +58,19 @@
             this.panel1.Size = new System.Drawing.Size(287, 640);
             this.panel1.TabIndex = 0;
             // 
-            // flowLayoutPanel1
+            // panelMenu
             // 
-            this.flowLayoutPanel1.Controls.Add(this.button2);
-            this.flowLayoutPanel1.Controls.Add(this.button1);
-            this.flowLayoutPanel1.Controls.Add(this.button3);
-            this.flowLayoutPanel1.Controls.Add(this.button4);
-            this.flowLayoutPanel1.Controls.Add(this.button5);
-            this.flowLayoutPanel1.Controls.Add(this.button6);
-            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 190);
-            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(287, 450);
-            this.flowLayoutPanel1.TabIndex = 2;
-            // 
-            // user
-            // 
-            this.user.AutoSize = true;
-            this.user.Font = new System.Drawing.Font("Mistral", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.user.Location = new System.Drawing.Point(69, 148);
-            this.user.Name = "user";
-            this.user.Size = new System.Drawing.Size(114, 23);
-            this.user.TabIndex = 1;
-            this.user.Text = "Nguyen Van Dai";
-            // 
-            // panel2
-            // 
-            this.panel2.BackColor = System.Drawing.Color.White;
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel2.Location = new System.Drawing.Point(287, 0);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(937, 640);
-            this.panel2.TabIndex = 1;
+            this.panelMenu.Controls.Add(this.button2);
+            this.panelMenu.Controls.Add(this.button1);
+            this.panelMenu.Controls.Add(this.button3);
+            this.panelMenu.Controls.Add(this.button4);
+            this.panelMenu.Controls.Add(this.button5);
+            this.panelMenu.Controls.Add(this.button6);
+            this.panelMenu.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panelMenu.Location = new System.Drawing.Point(0, 190);
+            this.panelMenu.Name = "panelMenu";
+            this.panelMenu.Size = new System.Drawing.Size(287, 450);
+            this.panelMenu.TabIndex = 2;
             // 
             // button2
             // 
@@ -125,6 +108,7 @@
             this.button1.Text = "Tình trạng phòng";
             this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button3
             // 
@@ -143,6 +127,7 @@
             this.button3.Text = "Thông tin đặt phòng";
             this.button3.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
@@ -161,6 +146,7 @@
             this.button4.Text = "Thông tin dịch vụ";
             this.button4.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button5
             // 
@@ -179,6 +165,7 @@
             this.button5.Text = "Thông tin lưu trú";
             this.button5.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // button6
             // 
@@ -197,6 +184,18 @@
             this.button6.Text = "Đăng ký dịch vụ";
             this.button6.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // user
+            // 
+            this.user.AutoSize = true;
+            this.user.BackColor = System.Drawing.Color.Yellow;
+            this.user.Font = new System.Drawing.Font("Mistral", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.user.Location = new System.Drawing.Point(69, 148);
+            this.user.Name = "user";
+            this.user.Size = new System.Drawing.Size(114, 23);
+            this.user.TabIndex = 1;
+            this.user.Text = "Nguyen Van Dai";
             // 
             // pictureBox1
             // 
@@ -207,6 +206,24 @@
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.Controls.Add(this.panelCallForm);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(287, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(937, 640);
+            this.panel2.TabIndex = 1;
+            // 
+            // panelCallForm
+            // 
+            this.panelCallForm.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelCallForm.Location = new System.Drawing.Point(0, 0);
+            this.panelCallForm.Name = "panelCallForm";
+            this.panelCallForm.Size = new System.Drawing.Size(937, 640);
+            this.panelCallForm.TabIndex = 0;
             // 
             // Home
             // 
@@ -219,8 +236,9 @@
             this.Text = "Home";
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
-            this.flowLayoutPanel1.ResumeLayout(false);
+            this.panelMenu.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -230,7 +248,7 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.FlowLayoutPanel panelMenu;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label user;
         private System.Windows.Forms.Button button1;
@@ -238,5 +256,6 @@
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Panel panelCallForm;
     }
 }
--- Hotel/Home.cs
+++ Hotel/Home.cs
@@ -19,7 +19,73 @@ namespace Hotel
 
         private void button2_Click(object sender, EventArgs e)
         {
+            ActivateButton(sender);
+        }
+        void callForm(Form childForm)
+        {
+            if (activeform != null)
+                activeform.Close();
+            activeform = childForm;
+            childForm.TopLevel = false;
+            childForm.FormBorderStyle = FormBorderStyle.None;
+            childForm.Dock = DockStyle.Fill;
+            panelCallForm.Controls.Add(childForm);
+            panelCallForm.Tag = childForm;
+            childForm.BringToFront();
+            childForm.Show();
+        }
+        private Form activeform = null;
+        private Button currentButton;
+        private void ActivateButton(object btnSender)
+        {
+            if (btnSender != null)
+            {
+                if (currentButton != (Button)btnSender)
+                {
+                    DisableButton();
+                    Color color = ColorTranslator.FromHtml("#FFFFFF");
+                    currentButton = (Button)btnSender;
+                    currentButton.BackColor = color;
+                    
+                }
+            }
+        }
+        private void DisableButton()
+        {
+            foreach (Control previousBtn in panelMenu.Controls)
+            {
+                if (previousBtn.GetType() == typeof(Button))
+                {
+                    previousBtn.BackColor = Color.FromArgb(46, 51, 73);
+                    
+                }
+            }
+        }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender);
         }
     }
+    
 }
